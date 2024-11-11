3edb591fe843e1693b8f07f4548f1d53a66d7b1d
Neha <neha06012005@gmail.com>
2024-11-11 15:06:11 +0530

    Update Resume.js

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -12,9 +12,15 @@ const Resume = () => {
    const [achievementData, setAchievementData] = useState(false);
   return (
     <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
-      <div className="flex justify-center items-center text-center">
-        <Title title="2+ YEARS OF EXPERIENCE" des="My Resume" />
-      </div>
+     <div className="flex justify-center items-center text-center">
+  <a
+    href="https://drive.google.com/file/d/1-PVy9ehPOahSqqsMa8Ffk7XB4TZsoEzh/view?usp=drive_link" // Updated link for direct download
+    download="Neha_Resume.pdf" // Optional: set the downloaded file name
+    className="ml-4 text-designColor hover:underline">
+    <Title title="2+ YEARS OF EXPERIENCE" des="My Resume" />
+  </a>
+</div>
+
       <div>
         <ul className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
           <li
